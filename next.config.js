cb5ba08701059dfa7d035114ab579083db8366a6
ninest <parth.kabra@gmail.com>
2021-12-29 03:22:13 +0800

    setup redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  async redirects() {
+    return [
+      {
+        source: "/current",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/sonic-pi-vscode",
+        destination: "/vscode/sonic-pi",
+        permanent: true,
+      },
+      {
+        source: "/project/nextbus",
+        destination: "/project/nextbussg",
+        permanent: true,
+      },
+      {
+        source: "/project/nsr",
+        destination: "/project/national-service-resources",
+        permanent: true,
+      },
+      {
+        source: "/embed-google-forms",
+        destination: "/html/google-forms-embed",
+        permanent: true,
+      },
+      {
+        source: "/nextjs/non-file-based-routing",
+        destination: "/nextjs/custom-routing",
+        permanent: true,
+      },
+    ];
+  },
 };
